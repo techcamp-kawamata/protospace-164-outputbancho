b474d36da2fa3c4e8cb1df2baa55697bc8cbd53a
techcamp-kawamata <155680479+techcamp-kawamata@users.noreply.github.com>
2024-01-19 17:42:15 +0900

    ビューファイルの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: "prototypes#index"
-    resources :items,only: [:index, :new, :create]
+    resources :users
+    resources :prototypes, only: [:index, :new, :create]
+    resources :comments
 end
