0032e81e91952b5312e99e1f703763cd9dfe0fd4
techcamp-kawamata <155680479+techcamp-kawamata@users.noreply.github.com>
2024-01-19 18:28:29 +0900

    ログインかログインでないかでの遷移

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,7 @@
 class PrototypesController < ApplicationController
+
+  before_action :move_to_index, except: [:index, :show]
+
   def index
   end
 
@@ -17,4 +20,9 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end
